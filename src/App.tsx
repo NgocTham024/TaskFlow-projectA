30272c39ec6b14fa91f9f63f3d868969f2b9f8f0
Tô Thị Ngọc Thắm <ttngoctham.0108@gmail.com>
2026-08-14 12:58:31 +0700

    fix conflict App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import Board from './components/board/Board';
 
 function App() {
-  return(
-    <Board/>
-  );
+  
 }   
 
 export default App;
